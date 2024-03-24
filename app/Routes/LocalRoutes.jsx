77c66f24d122b0adff3e72d895835ce07f3e5914
Sarkheel786Javed <sarkheel@kodware.net>
2024-03-24 05:27:41 +0500

    Initialize project using Create React App

---
+++ app/Routes/LocalRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LocalRoutes() {
+  return (
+    <div>LocalRoutes</div>
+  )
+}
+
+export default LocalRoutes
---
+++ app/layout/Sidebar/SidebarItems/SideBarItems.jsx
@@ -0,0 +1,59 @@
+export const dataSide = [
+    {
+        navigate: '/',
+        title: "Home",
+        icon:<></>,
+        subItems: [],
+    },
+    {
+        navigate: '/',
+        title: "Topics",
+        icon:<></>,
+        subItems: [
+            { navigate: '/', title: "React" ,icon:''}
+        ],
+    },
+    {
+        navigate: '/',
+        title: "Home",
+        icon:<></>,
+        subItems: [],
+    },
+    {
+        navigate: '/',
+        title: "Topics",
+        icon:<></>,
+        subItems: [
+            { navigate: '/', title: "React" ,icon:''}
+        ],
+    },
+    {
+        navigate: '/',
+        title: "Home",
+        icon:<></>,
+        subItems: [],
+    },
+    {
+        navigate: '/',
+        title: "Topics",
+        icon:<></>,
+        subItems: [
+            { navigate: '/', title: "React" ,icon:''}
+        ],
+    },
+    {
+        navigate: '/',
+        title: "Home",
+        icon:<></>,
+        subItems: [],
+    },
+    {
+        navigate: '/',
+        title: "Topics",
+        icon:<></>,
+        subItems: [
+            { navigate: '/', title: "React" ,icon:''}
+        ],
+    },
+   
+  ];
