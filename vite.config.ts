b68e5a46e187388ae8adbe146152e42ae7d37710
sarkheel21429 <your_email@example.com>
2024-06-11 05:10:45 +0500

    xzbzc

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Sarkheel786Javed/",
+  base:"/Sarkheel786Javed/" ,
   plugins: [react()],
 })
